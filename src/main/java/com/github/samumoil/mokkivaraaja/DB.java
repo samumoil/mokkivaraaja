44179fdcd655b96efe60ab5c8c2babccf2f0d69f
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2025-04-20 11:45:07 +0300

    Added javafx and refactored db to use a HikariCP datasource

--- src/main/java/com/github/samumoil/mokkivaraaja/DB.java
+++ src/main/java/com/github/samumoil/mokkivaraaja/DB.java
@@ -1,45 +1,26 @@
 package com.github.samumoil.mokkivaraaja;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DB implements AutoCloseable {
-  private final Connection dbConnection;
+public class DB {
+  private final DataSource dataSource;
 
-  DB() {
-    this.dbConnection = getConnection();
-  }
-
-  private Connection getConnection() {
-    DatabaseConfig dbConfig = new DatabaseConfig();
-    String jdbcUrl = dbConfig.getDbUrl();
-    String user = dbConfig.getDbUser();
-    String password = dbConfig.getDbPassword();
-    try {
-      return DriverManager.getConnection(jdbcUrl, user, password);
-    } catch (SQLException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
-    }
-  }
-
-  @Override
-  public void close() throws SQLException {
-    if (dbConnection != null && !dbConnection.isClosed()) {
-      dbConnection.close();
-    }
+  DB(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   public List<String> getNames() {
     List<String> names = new ArrayList<>();
-    try {
-      PreparedStatement st = dbConnection.prepareStatement("SELECT * FROM test");
-      ResultSet rs = st.executeQuery();
+    try (Connection dbConnection = dataSource.getConnection();
+         PreparedStatement st = dbConnection.prepareStatement("SELECT * FROM test");
+         ResultSet rs = st.executeQuery()) {
+
       while (rs.next()) {
         names.add(rs.getString("name"));
       }
-
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
